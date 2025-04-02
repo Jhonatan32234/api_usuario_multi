220f2ccc5660e47d5d3c153a197168ceda397a98
Jhonatan <233421@ids.upchiapas.edu.mx>
2025-04-02 12:50:21 -0600

    cambio de cors

--- cmd/infraestructure/routes/userProductRoutes.go
+++ cmd/infraestructure/routes/userProductRoutes.go
@@ -1,31 +1,38 @@
 package routes
 
 import (
-	"apisuario/cmd/application/usecases"
-	"apisuario/cmd/domain/repositories"
-	"apisuario/cmd/infraestructure/controllers"
+    "apisuario/cmd/application/usecases"
+    "apisuario/cmd/domain/repositories"
+    "apisuario/cmd/infraestructure/controllers"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    "gorm.io/gorm"
 )
 
 func SetupRouter(db *gorm.DB) *gin.Engine {
     router := gin.Default()
 
-    // Aplica CORS antes de definir las rutas
-    router.Use(cors.Default()) // Esto habilita CORS con la configuración predeterminada (permite todos los orígenes)
+    // Middleware para manejar solicitudes OPTIONS y descartarlas con un 200
+    router.Use(func(c *gin.Context) {
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(200)
+            return
+        }
+        c.Next()
+    })
 
-    // Si deseas más configuraciones personalizadas, puedes usar cors.New(), por ejemplo:
-    // router.Use(cors.New(cors.Config{
-    //     AllowOrigins: []string{"http://localhost:3000"},  // Solo permite ciertos orígenes
-    //     AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
-    //     AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
-    // }))
+    // Configuración de CORS personalizada
+    router.Use(cors.New(cors.Config{
+        AllowAllOrigins:  true,
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+    }))
 
     // Repositorios y casos de uso
     userProductRepo := repositories.NewUserRepositoryDB(db)
-
     connectUserProductUseCase := usecases.NewConnectUserProductUseCase(userProductRepo)
     verifyUserDeviceUseCase := usecases.NewVerifyUserDeviceUseCase(userProductRepo)
 
